d2e19dababfb8a2bc7887380af4c6114ad68b9f3
tmacjx <381221780@qq.com>
2018-09-26 17:55:06 +0800

    Update homebrew_install.sh

--- script/homebrew_install.sh
+++ script/homebrew_install.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-array=( autoconf git libevent libunistring nettle pkg-config redis wget ctags gmp libffi lua openssl pyenv rtmpdump xz emacs gnutls libtasn1 makedepend p11-kit readline vim )
+array=( 
+    autoconf 
+    git 
+    libevent 
+    libunistring 
+    nettle 
+    pkg-config 
+    redis 
+    wget
+    ctags 
+    gmp 
+    libffi 
+    lua 
+    openssl 
+    pyenv 
+    rtmpdump 
+    xz 
+    emacs 
+    gnutls 
+    libtasn1 
+    makedepend 
+    p11-kit 
+    readline 
+    vim 
+    )
 for i in "${array[@]}"
 do
     brew install $i
