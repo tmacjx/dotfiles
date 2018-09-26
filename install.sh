a4cad1bef21561e749afb54b6961d075a104fbca
tmacjx <381221780@qq.com>
2018-09-26 16:29:58 +0800

    script fix

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 # bash
-ln -s ~/.dotfiles/config/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/config/aliases ~/.aliases
+ln -s ~/dotfiles/config/bash_profile ~/.bash_profile
 
 # git 
-ln -s ~/.dotfiles/config/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/config/gitconfig ~/.gitconfig
 
 # vim
-ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
